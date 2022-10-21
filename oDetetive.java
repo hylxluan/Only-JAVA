a90b41c037d5cb1215fea57c8628834f205b19ca
luanhylx <113941286+hylxluan@users.noreply.github.com>
2022-10-21 11:46:20 -0300

    Detetive

--- oDetetive.java
+++ oDetetive.java
@@ -1,14 +1,3 @@
-/*
-Objetivo: utilizando listas, elabore um programa que faça 5 perguntas para uma pessoa sobre o crime, as perguntas são:
-
-1- "Telefonou para a vítima?"
-2- "Esteve no local do crime?"
-3- "Mora perto da vítima?"
-4- "Devia para a vítima?"
-5- "Já trabalhou com a vítima?"
-
-Se a pessoa responder positivamente a 2 questões ela deve ser classificada como "Suspeita", entre 3 e 4 como "Cúmplice"
-e 5 como "Assassina". Caso contrário, será declarada como "Inocente".*/
 import java.util.*;
 public class oDetetive {
     public static void main(String[] args) {
